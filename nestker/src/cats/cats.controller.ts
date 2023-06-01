a8b1a57ff39ba5a40fae6a2016b68967a388509f
aleksa-stajcic <aleksa@byteout.com>
2023-06-01 22:54:17 +0200

    docker: this should now work out of the box

--- nestker/src/cats/cats.controller.ts
+++ nestker/src/cats/cats.controller.ts
@@ -2,9 +2,9 @@ import { Controller, Get, Req } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
-    @Get()
-    findAll(@Req() request: Request): string {
-        // console.log(request);
-        return 'all cats';
-    }
+  @Get()
+  findAll(@Req() request: Request): string {
+    // console.log(request);
+    return 'all cats are here';
+  }
 }
